388310eebb01c1bb80ce1d2d3b78efa1a9e244bd
Manuel Bouza <manuel@bouza.ch>
2019-02-05 10:44:11 +0100

    Update webpack config
    - write bundle to `/build`
    - add support for SASS
    - improve options view as a proof o concept for styling

--- src/js/bubble.js
+++ src/js/bubble.js
@@ -1,16 +1,17 @@
-import { createElement } from 'react'
-import ReactDOM from 'react-dom'
-import Bubble from './components/Bubble'
+import { createElement } from "react";
+import ReactDOM from "react-dom";
+import Bubble from "./components/Bubble";
+import "../css/main.scss";
 
 // don't initiate bubble twice
-if (!document.querySelector('#moco-bx-container')) {
-  const domContainer = document.createElement('div')
-  domContainer.setAttribute('id', 'moco-bx-container')
-  document.body.appendChild(domContainer)
+if (!document.querySelector("#moco-bx-container")) {
+  const domContainer = document.createElement("div");
+  domContainer.setAttribute("id", "moco-bx-container");
+  document.body.appendChild(domContainer);
 
-  const domBubble = document.createElement('div')
-  domBubble.setAttribute('id', 'moco-bx-bubble')
-  document.body.appendChild(domBubble)
+  const domBubble = document.createElement("div");
+  domBubble.setAttribute("id", "moco-bx-bubble");
+  document.body.appendChild(domBubble);
 
-  ReactDOM.render(createElement(Bubble), domBubble)
+  ReactDOM.render(createElement(Bubble), domBubble);
 }
