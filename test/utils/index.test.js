20359a949300989f49b73984b4ac512b3945266d
Manuel Bouza <manuel@bouza.ch>
2019-03-07 20:31:09 +0100

    Use TimeInputParser to parse hours input

--- test/utils/index.test.js
+++ test/utils/index.test.js
@@ -1,17 +1,16 @@
 import { projects } from "../data"
 import {
-  findLastProject,
-  findLastTask,
-  groupedProjectOptions,
-  secondsFromHours
+  findProject,
+  findTask,
+  groupedProjectOptions
 } from "../../src/js/utils"
 import { map } from "lodash/fp"
 
 describe("utils", () => {
-  describe("findLastProject", () => {
+  describe("findProject", () => {
     it("finds an existing project", () => {
       const options = groupedProjectOptions(projects)
-      const project = findLastProject(944837106)(options)
+      const project = findProject(944837106)(options)
       expect(project.value).toEqual(944837106)
       expect(project.label).toEqual("Support")
       expect(project.customerName).toEqual("MOCO APP")
@@ -20,35 +19,35 @@ describe("utils", () => {
 
     it("returns undefined if project is not found", () => {
       const options = groupedProjectOptions(projects)
-      const project = findLastProject(123)(options)
+      const project = findProject(123)(options)
       expect(project).toBe(undefined)
     })
 
     it("returns undefined for undefined id", () => {
       const options = groupedProjectOptions(projects)
-      const project = findLastProject(undefined)(options)
+      const project = findProject(undefined)(options)
       expect(project).toBe(undefined)
     })
   })
 
-  describe("findLastTask", () => {
+  describe("findTask", () => {
     it("find an existing task", () => {
       const options = groupedProjectOptions(projects)
-      const project = findLastProject(944837106)(options)
-      const task = findLastTask(2506050)(project)
+      const project = findProject(944837106)(options)
+      const task = findTask(2506050)(project)
       expect(task.value).toEqual(2506050)
       expect(task.label).toEqual("(Calls)")
     })
 
     it("returns undefined if task is not found", () => {
       const options = groupedProjectOptions(projects)
-      const project = findLastProject(944837106)(options)
-      const task = findLastTask(123)(project)
+      const project = findProject(944837106)(options)
+      const task = findTask(123)(project)
       expect(task).toBe(undefined)
     })
 
     it("returns undefined for undefined project", () => {
-      const task = findLastTask(2506050)(undefined)
+      const task = findTask(2506050)(undefined)
       expect(task).toBe(undefined)
     })
   })
@@ -61,29 +60,6 @@ describe("utils", () => {
         "MOCO APP",
         "sharoo"
       ])
-    })
-  })
-
-  describe("secondsFromHours", () => {
-    it("converts a single number to seconds", () => {
-      expect(secondsFromHours('1')).toEqual(3600)
-      expect(secondsFromHours('2')).toEqual(7200)
-    })
-
-    it("treats number after dot as fractional hours", () => {
-      expect(secondsFromHours('1.3')).toEqual(4680)
-      expect(secondsFromHours('2.8')).toEqual(10080)
-    })
-
-    it("treats number after colon as minutes", () => {
-      expect(secondsFromHours('1:20')).toEqual(4800)
-      expect(secondsFromHours('2:50')).toEqual(10200)
-    })
-
-    it("treats invalid numbers as zero", () => {
-      expect(secondsFromHours(undefined)).toEqual(0)
-      expect(secondsFromHours('ab')).toEqual(0)
-      expect(secondsFromHours('a:i')).toEqual(0)
     })
   })
 })
