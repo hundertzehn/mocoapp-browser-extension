0700804e03792c9d5f62c99ad43a780f02965e77
Manuel Bouza <manuel@bouza.ch>
2019-03-20 00:19:07 +0100

    Only report errors from own sources

--- src/js/utils/notifier.js
+++ src/js/utils/notifier.js
@@ -1,14 +1,25 @@
 import React from "react"
 import bugsnag from "@bugsnag/js"
 import bugsnagReact from "@bugsnag/plugin-react"
+import { includes } from "lodash/fp"
 
 const { version: appVersion } = chrome.runtime.getManifest()
 
+const filterReport = report => {
+  const { version } = chrome.runtime.getManifest()
+  const scripts = ["background", "content", "options", "popup"].map(
+    file => `${chrome.extension.getURL(file)}.${version}.js`
+  )
+
+  return scripts.some(script => report.stacktrace.some(includes(script)))
+}
+
 const bugsnagClient = bugsnag({
   apiKey: "da6caac4af70af3e4683454b40fe5ef5",
-  appVersion
-  // releaseStage: process.env.NODE_ENV,
-  // notifyReleaseStages: ["production"]
+  appVersion,
+  beforeSend: filterReport,
+  releaseStage: process.env.NODE_ENV,
+  notifyReleaseStages: ["production"]
 })
 
 bugsnagClient.use(bugsnagReact, React)
