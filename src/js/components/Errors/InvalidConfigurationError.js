e8ab05295ec94a946771ae2e2f85d0c929a30e84
manubo <manuel@bouza.ch>
2019-09-28 09:23:16 +0200

    Update view layouts and content

--- src/js/components/Errors/InvalidConfigurationError.js
+++ src/js/components/Errors/InvalidConfigurationError.js
@@ -3,25 +3,23 @@ import settingsUrl from "images/settings.png"
 
 const InvalidConfigurationError = () => (
   <div className="moco-bx-error-container">
-    <h1>Bitte Einstellungen aktualisieren</h1>
-    <ol>
-      <li>Internetadresse eintragen</li>
-      <li>Persönlichen API-Schlüssel eintragen</li>
-    </ol>
-    <button
-      className="moco-bx-btn"
-      onClick={() => chrome.runtime.sendMessage({ type: "openOptions" })}
-    >
-      Einstellungen öffnen
-    </button>
-    <br />
-    <br />
+    <h1>MOCO verbinden</h1>
+    <p>
+      Dazu trägst Du in den Einstellungen Deine Account-Internetadresse und Deinen API-Schlüssel
+      ein.
+    </p>
     <img
       src={chrome.extension.getURL(settingsUrl)}
       alt="Browser extension configuration settings"
       style={{ cursor: "pointer" }}
       onClick={() => chrome.runtime.sendMessage({ type: "openOptions" })}
     />
+    <button
+      className="moco-bx-btn"
+      onClick={() => chrome.runtime.sendMessage({ type: "openOptions" })}
+    >
+      Weiter zu den Einstellungen
+    </button>
   </div>
 )
 
