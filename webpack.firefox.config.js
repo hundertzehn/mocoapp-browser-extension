41656756fd1a3fdfba5c4df7951e2e909a5f3d02
Manuel Bouza <manuel@bouza.ch>
2020-06-04 23:28:55 +0200

    Update copy-webpack-plugin

--- webpack.firefox.config.js
+++ webpack.firefox.config.js
@@ -4,44 +4,42 @@ const { compact } = require("lodash/fp")
 
 const baseConfig = require("./webpack.base.config")
 
-module.exports = env => {
+module.exports = (env) => {
   const config = baseConfig(env)
 
   config.plugins.unshift(
-    new CopyWebpackPlugin([
-      {
-        from: "src/manifest.json",
-        transform: function(content, _path) {
-          const manifest = JSON.parse(
-            content
-              .toString()
-              .replace(/\[version\]/g, process.env.npm_package_version)
-          )
-          return Buffer.from(
-            JSON.stringify({
-              ...manifest,
-              permissions: compact([
-                ...manifest.permissions,
-                env.NODE_ENV === "development"
-                  ? "http://*.mocoapp.localhost/*"
-                  : null
-              ]),
-              options_ui: {
-                ...manifest.options_ui,
-                browser_style: true
-              },
-              browser_specific_settings: {
-                gecko: {
-                  id: process.env.APPLICATION_ID || `{${uuidv4()}}`
-                }
-              },
-              description: process.env.npm_package_description,
-              version: process.env.npm_package_version
-            })
-          )
-        }
-      }
-    ])
+    new CopyWebpackPlugin({
+      pattern: [
+        {
+          from: "src/manifest.json",
+          transform: function (content, _path) {
+            const manifest = JSON.parse(
+              content.toString().replace(/\[version\]/g, process.env.npm_package_version),
+            )
+            return Buffer.from(
+              JSON.stringify({
+                ...manifest,
+                permissions: compact([
+                  ...manifest.permissions,
+                  env.NODE_ENV === "development" ? "http://*.mocoapp.localhost/*" : null,
+                ]),
+                options_ui: {
+                  ...manifest.options_ui,
+                  browser_style: true,
+                },
+                browser_specific_settings: {
+                  gecko: {
+                    id: process.env.APPLICATION_ID || `{${uuidv4()}}`,
+                  },
+                },
+                description: process.env.npm_package_description,
+                version: process.env.npm_package_version,
+              }),
+            )
+          },
+        },
+      ],
+    }),
   )
 
   return config
