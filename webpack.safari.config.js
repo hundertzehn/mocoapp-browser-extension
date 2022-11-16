81640f4757eb960ddf7f554ed67ac35c3a97d611
Daniel Pangerl <daniel@dpangerl.de>
2022-11-16 18:19:53 +0100

    Adds new configuration for safari browser extension

---
+++ webpack.safari.config.js
@@ -0,0 +1,39 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const { compact } = require("lodash/fp")
+
+const baseConfig = require("./webpack.base.config")
+
+module.exports = (env) => {
+  const config = baseConfig(env)
+
+  config.plugins.unshift(
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/manifest.safari.json",
+          to: "manifest.json",
+          transform: function (content, _path) {
+            const manifest = JSON.parse(
+              content.toString().replace(/\[version\]/g, process.env.npm_package_version),
+            )
+            return Buffer.from(
+              JSON.stringify({
+                ...manifest,
+                host_permissions: compact([
+                  ...manifest.host_permissions,
+                  env.NODE_ENV === "development" && process.env.USE_LOCAL_MOCO === "true"
+                    ? "http://*.mocoapp.localhost/*"
+                    : null,
+                ]),
+                description: process.env.npm_package_description,
+                version: process.env.npm_package_version,
+              }),
+            )
+          },
+        },
+      ],
+    }),
+  )
+
+  return config
+}
