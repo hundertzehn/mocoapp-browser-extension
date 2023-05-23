8a229b9f2615f227a413a14d49a14b1a66a3c97b
Manuel Bouza <manuel@bouza.ch>
2023-05-23 16:31:12 +0200

    Move openproject service to remoteServicesCommunity

--- src/js/remoteServicesCommunity.js
+++ src/js/remoteServicesCommunity.js
@@ -39,4 +39,47 @@ export default {
     projectId: projectIdentifierBySelector('meta[name="current-bucket-name"]', "content"),
     allowHostOverride: true,
   },
+
+  openproject: {
+    name: "openproject",
+    host: "https://:org.openproject.com",
+    urlPatterns: [
+      ":host:/projects/:project/work_packages/:id(/*)",
+      ":host:/projects/:project/work_packages/details/:id(/*)",
+      ":host:/work_packages/:id(/*)",
+      ":host:/work_packages/details/:id(/*)",
+    ],
+    description: (document) => {
+      let subject =
+        document.querySelector(".work-packages--details--subject")?.textContent?.trim() || ""
+      let subjectId =
+        document.querySelector(".work-packages--info-row")?.firstChild?.textContent?.trim() || ""
+
+      if (subjectId) {
+        subjectId = "OP " + subjectId
+      }
+      if (subject && subjectId) {
+        subject = subjectId + " " + subject
+      }
+
+      return subject || subjectId
+    },
+    projectId: (document) => {
+      // ":project" in URL can be project name or OP internal project ID. Therefore, it cannot be used.
+      return (
+        projectIdentifierBySelector(".-project-context a")(document) ||
+        projectIdentifierBySelector("#projects-menu")(document)
+      )
+    },
+    projectLabel: (document) => {
+      // ":project" in URL can be project name or OP internal project ID. Therefore, it cannot be used.
+      return (
+        (
+          document.querySelector(".-project-context a") || document.querySelector("#projects-menu")
+        )?.textContent?.trim() || ""
+      )
+    },
+    allowHostOverride: true,
+    position: { left: "calc(2rem + 5px)" },
+  },
 }
