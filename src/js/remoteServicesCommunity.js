882eb7b4f6294b0c636d7853112b15f63e72f49d
mariusheine <m.heine@geprog.com>
2024-03-22 22:10:46 +0000

    configure gitlab properly

--- src/js/remoteServicesCommunity.js
+++ src/js/remoteServicesCommunity.js
@@ -5,15 +5,22 @@ export default {
     name: "gitlab",
     host: "https://gitlab.com",
     urlPatterns: [
-      ":host:/:org/:group(/*)/:projectId/-/issues/:id(#note_:noteId)",
-      ":host:/:org(/*)/:projectId/-/issues/:id(#note_:noteId)",
-      ":host:/:org/:group(/*)/:projectId/-/merge_requests/:id(#note_:noteId)",
-      ":host:/:org(/*)/:projectId/-/merge_requests/:id(#note_:noteId)",
+      ":host:/:org/:group(/*)/:projectId/-/issues/:issueId(#note_:noteId)",
+      ":host:/:org(/*)/:projectId/-/issues/:issueId(#note_:noteId)",
+      ":host:/:org/:group(/*)/:projectId/-/merge_requests/:mergeRequestId(#note_:noteId)",
+      ":host:/:org(/*)/:projectId/-/merge_requests/:mergeRequestId(#note_:noteId)",
     ],
-    description: (document, service, { id, noteId: _noteId }) => {
+    description: (document, service, { issueId, mergeRequestId, noteId: _noteId }) => {
       const title = document.querySelector(".detail-page-description .title")?.textContent?.trim()
+      const id = undefiend;
+      if (issueId) {
+        id = `#${issueId}`; // add '#' as prefix so that we can identify it as an issue
+      } else if (mergeRequestId) {
+        id = `!${mergeRequestId}`; // add '!' as prefix so that we can identify it as an merge request
+      }
       return `#${id} ${title || ""}`.trim()
     },
+    projectId: projectIdentifierBySelector(".js-breadcrumb-item-text"),
     allowHostOverride: true,
   },
 
